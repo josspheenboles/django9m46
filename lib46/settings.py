649afd9723823a7aba6eaed6d66493a2ddf2e591
josspheenboles <eng.jospheenboles@gmail.com>
2026-04-19 11:57:04 +0200

    templates

--- lib46/settings.py
+++ lib46/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'lib46.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
