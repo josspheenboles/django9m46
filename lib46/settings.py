c37c3f6dc3caccccc43c8d0322e5d09756381f8a
josspheenboles <eng.jospheenboles@gmail.com>
2026-04-19 10:22:35 +0200

    install app

--- lib46/settings.py
+++ lib46/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'catagory',
+    'book.apps.BookConfig' ,#professional
 ]
 
 MIDDLEWARE = [
