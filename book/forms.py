6c690807afa71fb457995997214ce4d7d6669446
josspheenboles <eng.jospheenboles@gmail.com>
2026-04-20 00:59:47 +0200

    formsModel

--- book/forms.py
+++ book/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from catagory.models import Catagory
+from book.models import Book
 class BookForm(forms.Form):
     name = forms.CharField(max_length=100,required=True)
     description = forms.CharField()
@@ -7,3 +8,8 @@ class BookForm(forms.Form):
     price = forms.DecimalField(max_digits=5, decimal_places=2)
     publish_date = forms.DateField()
     catagory = forms.ChoiceField(choices=[(cat.id,cat.name) for cat in Catagory.objects.all()])
+
+class BookFormModel(forms.ModelForm):
+    class Meta:
+        model=Book
+        Feilds=['__all__']
